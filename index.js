9dc7f2d48ad679eeb8bc28b3cd310b2925f76c84
blSirius <siriusblackazka@gmail.com>
2023-11-15 18:21:41 +0700

    15/11

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+
+app.use(cors())
 
 app.get("/", async (req, res) => {
     res.send('hello peter')
