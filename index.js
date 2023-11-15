6d1c0d63a19326ea2dffc146a396771c61e8de93
blSirius <siriusblackazka@gmail.com>
2023-11-15 12:37:09 +0700

    15/11

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-mongoose.connect('mongodb://localhost:27017/Auth', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).catch(error => console.error("MongoDB connection error:", error));
 
 app.get('/', async (req, res) => {
     try {
@@ -24,10 +15,6 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
