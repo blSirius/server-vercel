a7421b98f088194d2c5b3cdd2f9b5224eb37f47e
blSirius <siriusblackazka@gmail.com>
2023-11-15 14:59:25 +0700

    15/11

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
-const express = require('express')
-const cors = require('cors')
-// const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Check if the connection is successful
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Define a Mongoose Model
+// const ExampleSchema = new mongoose.Schema({
+//   name: String,
+//   age: Number,
+// });
+
+// const ExampleModel = mongoose.model('Example', ExampleSchema);
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// mongoose.connect('mongodb://localhost:27017/< Your database name >', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
+// Routes
+app.get('/', async (req, res) => {
+    try {
+        res.send('connected');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-app.get('/',(req,res)=>{
-    res.send(`Server is running on http://localhost:${PORT}`)
-})
-
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Internal Server Error');
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
