caebe89cd226e5d6609c49ec02d4cd557478e137
blSirius <siriusblackazka@gmail.com>
2023-11-15 17:46:22 +0700

    15/11

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//index.js
 require('dotenv').config(); // Import dotenv to load environment variables
 const express = require('express');
 const cors = require('cors');
@@ -13,12 +14,21 @@ app.use(cors());
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('MongoDB connected successfully');
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+})
+.catch((error) => {
+    console.error('MongoDB connection failed:', error);
 });
 
 app.get('/', async (req, res) => {
     res.send('hello peter');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
