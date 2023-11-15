4bb70953d474f5b0572b4e542be73179a43fb65a
blSirius <siriusblackazka@gmail.com>
2023-11-15 14:41:55 +0700

    15/11

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 // const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // mongoose.connect('mongodb://localhost:27017/< Your database name >', {
 //     useNewUrlParser: true,
@@ -18,10 +18,10 @@ app.get('/',(req,res)=>{
     res.send(`Server is running on http://localhost:${PORT}`)
 })
 
-// app.use((err, req, res, next) => {
-//     console.error(err.stack);
-//     res.status(500).send('Internal Server Error');
-// });
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
