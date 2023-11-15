6ddee09f78f5923302d16f80ba581bb2cba8033d
blSirius <siriusblackazka@gmail.com>
2023-11-15 14:40:39 +0700

    15/11

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 // const cors = require('cors')
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,10 +9,10 @@ const PORT = process.env.PORT || 5000;
 // app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/< Your database name >', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// mongoose.connect('mongodb://localhost:27017/< Your database name >', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
 app.get('/',(req,res)=>{
     res.send(`Server is running on http://localhost:${PORT}`)
