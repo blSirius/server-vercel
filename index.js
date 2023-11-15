588d1cdcecfbdd5965f730d89d333d2f272951ab
blSirius <siriusblackazka@gmail.com>
2023-11-15 18:23:23 +0700

    15/11

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-require('dotenv').config()
-const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
 
-app.use(cors())
+// Use async function for connecting to MongoDB
+async function connectToDatabase() {
+    try {
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+        console.log("Connected to MongoDB");
+        return client;
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+        throw err; // Rethrow the error for proper handling
+    }
+}
+
+app.use(cors());
 
 app.get("/", async (req, res) => {
-    res.send('hello peter')
-})
+    res.send('Hello Peter');
+});
 
-client.connect(err => {
-    if(err){ console.error(err); return false;}
+// Move the MongoDB connection logic outside the route handler
+connectToDatabase().then((client) => {
+    app.locals.db = client.db(); // Store the MongoDB client in app.locals
     app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
+        console.log("Listening for requests on port", PORT);
+    });
+});
+
+// Handle graceful shutdown by closing the MongoDB connection
+process.on('SIGINT', async () => {
+    try {
+        await app.locals.db.client.close();
+        console.log('MongoDB connection closed.');
+        process.exit(0);
+    } catch (err) {
+        console.error('Error closing MongoDB connection:', err);
+        process.exit(1);
+    }
 });
