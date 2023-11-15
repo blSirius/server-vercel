f66758a17b7cce650c8a6e7fefde39194e6cc4e5
blSirius <siriusblackazka@gmail.com>
2023-11-15 15:09:35 +0700

    15/11

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Auth', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://siriusblackazka:azkabann@cluster0.ar5fu2f.mongodb.net/?retryWrites=true&w=majority/Auth', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
