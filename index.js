3b28c2d43c18ba3c758f3b68f07805b3db65b356
blSirius <siriusblackazka@gmail.com>
2023-11-15 22:19:28 +0700

    15/11

--- index.js
+++ index.js
@@ -1,48 +1,23 @@
-require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-const uri = process.env.MONGODB_URI;
-
-// Use async function for connecting to MongoDB
-async function connectToDatabase() {
-    try {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect();
-        console.log("Connected to MongoDB");
-        return client;
-    } catch (err) {
-        console.error("Error connecting to MongoDB:", err);
-        throw err; // Rethrow the error for proper handling
-    }
-}
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", async (req, res) => {
-    res.send('Hello Peter');
+mongoose.connect('mongodb+srv://siriusblackazka:azkabann@cluster0.mou1mqu.mongodb.net/Auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-// Move the MongoDB connection logic outside the route handler
-connectToDatabase().then((client) => {
-    app.locals.db = client.db(); // Store the MongoDB client in app.locals
-    app.listen(PORT, () => {
-        console.log("Listening for requests on port", PORT);
-    });
+app.get('/', (req, res) => {
+    res.send('Harry Potter')
 });
 
-// Handle graceful shutdown by closing the MongoDB connection
-process.on('SIGINT', async () => {
-    try {
-        await app.locals.db.client.close();
-        console.log('MongoDB connection closed.');
-        process.exit(0);
-    } catch (err) {
-        console.error('Error closing MongoDB connection:', err);
-        process.exit(1);
-    }
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
