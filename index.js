7b066bc9e4113ffd1e8cb643714743fab78e1da3
blSirius <siriusblackazka@gmail.com>
2023-11-15 17:12:30 +0700

    15/11

--- index.js
+++ index.js
@@ -1,55 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require('express')
+const cors = require('cors')
+// const mongoose = require('mongoose')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://siriusblackazka:azkabann@cluster0.ar5fu2f.mongodb.net/Auth/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
 
-// Check if the connection is successful
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
-
-// Define a Mongoose Model
-// const ExampleSchema = new mongoose.Schema({
-//   name: String,
-//   age: Number,
-// });
-
-// const ExampleModel = mongoose.model('Example', ExampleSchema);
-
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors())
 
-// Routes
+// mongoose.connect('mongodb+srv://siriusblackazka:azkabann@cluster0.ar5fu2f.mongodb.net/todos', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
 app.get('/', async (req, res) => {
-    try {
-        res.send('connected');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
+    res.send('hello peter')
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-});
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-module.exports = app;
